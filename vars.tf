f8b3d0bffb2c48b1b37e33e594f9ed3bd2230cbd
Pradeep K P <arayofblessing@gmail.com>
2020-05-07 06:55:55 +0000

    syntax corrected

--- vars.tf
+++ vars.tf
@@ -7,8 +7,8 @@ default = "ap-south-1"
 
 variable "AMIS"{
 type = map
-default {
-ap-sount-1 = "ami-0c5b1a88222ac79cb"
+default = {
+ap-south-1 = "ami-0c5b1a88222ac79cb"
 eu-west-1 = "ami-0dad359ff462124ca"
 us-east-1 = "ami-068663a3c619dd892"
 us-west-1 = "ami-075fd582acf0c0128"
