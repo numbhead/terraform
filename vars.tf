c350c69f4c890308d945e19fc70391e0240971d0
Pradeep K P <arayofblessing@gmail.com>
2020-05-07 06:48:49 +0000

    Corrected the syntax issues

--- vars.tf
+++ vars.tf
@@ -1,16 +1,16 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION"{
-type : string
-default : "ap-south-1"
+type = string
+default = "ap-south-1"
 }
 
 variable "AMIS"{
-type : map
+type = map
 default {
-ap-sount-1 : "ami-0c5b1a88222ac79cb"
-eu-west-1 : "ami-0dad359ff462124ca"
-us-east-1 : "ami-068663a3c619dd892"
-us-west-1 : "ami-075fd582acf0c0128"
+ap-sount-1 = "ami-0c5b1a88222ac79cb"
+eu-west-1 = "ami-0dad359ff462124ca"
+us-east-1 = "ami-068663a3c619dd892"
+us-west-1 = "ami-075fd582acf0c0128"
 }
 }
