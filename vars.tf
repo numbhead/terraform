37744353284763ac69c6eca80b388e89f1c058a1
Pradeep K P <arayofblessing@gmail.com>
2020-05-07 08:33:11 +0000

    Adding scripts to deploy nginx

--- vars.tf
+++ vars.tf
@@ -6,7 +6,7 @@ default = "ap-south-1"
 }
 
 variable "AMIS"{
-type = map
+type = map(string)
 default = {
 ap-south-1 = "ami-0c5b1a88222ac79cb"
 eu-west-1 = "ami-0dad359ff462124ca"
@@ -14,3 +14,16 @@ us-east-1 = "ami-068663a3c619dd892"
 us-west-1 = "ami-075fd582acf0c0128"
 }
 }
+
+variable "PATH_TO_PUBLIC_KEY" {
+default = "keys/mykey.pub"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+default = "keys/mykey"
+}
+
+variable "INSTANCE_USER_NAME" {
+default = "ubuntu"
+}
+
